9d8885453ddcac3e8f499b94e7307d96f78169d2
AndreaSciscioli <113891464+AndreaSciscioli@users.noreply.github.com>
2023-03-08 17:09:46 +0100

    commit

---
+++ src/main/java/co/develhope/websocket2/entities/ClientMessageDTO.java
@@ -0,0 +1,37 @@
+package co.develhope.websocket2.entities;
+
+public class ClientMessageDTO {
+    private String clientName;
+    private String clientAlert;
+    private String clientMessage;
+    public ClientMessageDTO() {}
+    public ClientMessageDTO(String clientName, String clientAlert, String clientMessage) {
+        this.clientName = clientName;
+        this.clientAlert = clientAlert;
+        this.clientMessage = clientMessage;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public void setClientName(String clientName) {
+        this.clientName = clientName;
+    }
+
+    public String getClientAlert() {
+        return clientAlert;
+    }
+
+    public void setClientAlert(String clientAlert) {
+        this.clientAlert = clientAlert;
+    }
+
+    public String getClientMessage() {
+        return clientMessage;
+    }
+
+    public void setClientMessage(String clientMessage) {
+        this.clientMessage = clientMessage;
+    }
+}
